fdb5359f534eb4714cfc4c7c1c4613bab44cc888
Juan Guerrero <jjguerrerog02@gmail.com>
2024-04-12 11:16:56 -0600

    3rd version, shaders added

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,42 +5,39 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
-
+import shaders.StaticShader;
 
 public class MainGameLoop {
-
 	public static void main(String[] args) {
-		
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
-		
+		StaticShader shader = new StaticShader();
 		float[] vertices = {
-		
-				-0.5f, 0.5f, 0f,   //v0
-				-0.5f, -0.5f, 0f,  //v1
-				0.5f, -0.5f, 0f,   //v2
-				0.5f, 0.5f, 0f,    //v3
-		};
-		
-		int[] indices = {
-		
-				0,1,3,  //top left triangle (v0, v1, v3)
-				3,1,2   //bottom right triangle (v3, v1, v2)
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, indices);
 
+				-0.5f, 0.5f, 0, // V0
+				-0.5f, -0.5f, 0, // V1
+				0.5f, -0.5f, 0, // V2
+				0.5f, 0.5f, 0 // V3
+
+		};
+		int[] indices = {
+
+				0, 1, 3, // Top left triangle (V0,V1,V3)
+				3, 1, 2 // Bottom right triangle (V3,V1,V2)
+
+		};
+		RawModel model = loader.loadToVAO(vertices, indices);
 		while (!Display.isCloseRequested()) {
-		
 			// game logic
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
-
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
-
 }
