492f7c8046bb9ebed821de22b2b970cf821b90a5
Juan Guerrero <jjguerrerog02@gmail.com>
2024-04-15 22:56:29 -0600

    6 version, rendered a blender obj

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -10,6 +10,7 @@ import models.TexturedModel;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -20,89 +21,13 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		float[] vertices = {
-				-0.5f, 0.5f, -0.5f,
-				-0.5f, -0.5f, -0.5f,
-				0.5f, -0.5f, -0.5f,
-				0.5f, 0.5f, -0.5f,
 
-				-0.5f, 0.5f, 0.5f,
-				-0.5f, -0.5f, 0.5f,
-				0.5f, -0.5f, 0.5f,
-				0.5f, 0.5f, 0.5f,
+		RawModel model = OBJLoader.loadObjModel("sphere", loader);
 
-				0.5f, 0.5f, -0.5f,
-				0.5f, -0.5f, -0.5f,
-				0.5f, -0.5f, 0.5f,
-				0.5f, 0.5f, 0.5f,
-
-				-0.5f, 0.5f, -0.5f,
-				-0.5f, -0.5f, -0.5f,
-				-0.5f, -0.5f, 0.5f,
-				-0.5f, 0.5f, 0.5f,
-
-				-0.5f, 0.5f, 0.5f,
-				-0.5f, 0.5f, -0.5f,
-				0.5f, 0.5f, -0.5f,
-				0.5f, 0.5f, 0.5f,
-
-				-0.5f, -0.5f, 0.5f,
-				-0.5f, -0.5f, -0.5f,
-				0.5f, -0.5f, -0.5f,
-				0.5f, -0.5f, 0.5f
-
-		};
-
-		float[] textureCoords = {
-
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0,
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0,
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0,
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0,
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0,
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0
-
-		};
-
-		int[] indices = {
-				0, 1, 3,
-				3, 1, 2,
-				4, 5, 7,
-				7, 5, 6,
-				8, 9, 11,
-				11, 9, 10,
-				12, 13, 15,
-				15, 13, 14,
-				16, 17, 19,
-				19, 17, 18,
-				20, 21, 23,
-				23, 21, 22
-
-		};
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-
-		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("sphere2"));
 		TexturedModel staticModel = new TexturedModel(model, texture);
 
-		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5),
+		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -10),
 				0, 0, 0, 1);
 
 		Camera camera = new Camera();
