08a225462d318a4f3cc505f97b8a5596c6ea61e4
Juan Guerrero <jjguerrerog02@gmail.com>
2024-05-01 14:03:15 -0600

    7.0 Improved obj, texture files working, football ball model being rendered, Entities classes created

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,50 +1,77 @@
 package engineTester;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import models.RawModel;
+import models.TexturedModel;
+import objConverter.ModelData;
+import objConverter.OBJFileLoader;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
-import entities.Camera;
-import entities.Entity;
-import models.RawModel;
-import models.TexturedModel;
-
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
 
 public class MainGameLoop {
+
 	public static void main(String[] args) {
+
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
+		
+		String fileName = "football";
+		ModelData data = OBJFileLoader.loadOBJ(fileName);
 
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
-
-		ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
-		TexturedModel staticModel = new TexturedModel(model, texture);
-
-		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -10),
-				0, 0, 0, 1);
-
-		Camera camera = new Camera();
-
-		while (!Display.isCloseRequested()) {
-			entity.increaseRotation(1, 1f, 0 );
+		RawModel treeModel = loader.loadToVAO(data.getVertices(), data.getTextureCoords(),
+		data.getNormals(),data.getIndices());
+		
+		TexturedModel staticModel = new TexturedModel(treeModel,
+		new ModelTexture(loader.loadTexture(fileName)));
+		
+		List<Entity> entities = new ArrayList<Entity>();
+		Random random = new Random();
+		for(int i=0;i<15;i++){
+			entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*70 - 35,2.5f,random.nextFloat() *-150 - 30),0,0,0,3));
+		}
+		
+		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
+		List<Terrain> terrains = new ArrayList<Terrain>();
+		terrains.add(new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass"))));
+		terrains.add(new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("grass"))));
+		terrains.add(new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass"))));
+		terrains.add(new Terrain(-1,0,loader,new ModelTexture(loader.loadTexture("grass"))));
+		
+		Camera camera = new Camera();	
+		MasterRenderer renderer = new MasterRenderer();
+		
+		while(!Display.isCloseRequested()){
 			camera.move();
-			// Game logic
-			renderer.prepare();
-			shader.start();
-			shader.loadViewMatrix(camera);
-			renderer.render(entity, shader);
-			shader.stop();
+			
+			for(Terrain terrain:terrains){
+				renderer.processTerrain(terrain);
+			}
+			for(Entity entity:entities){
+				renderer.processEntity(entity);
+				entity.increaseRotation(0, 1, 0);
+			}
+			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
-		shader.cleanUp();
+
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
+
 	}
+
 }
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -2,30 +2,30 @@ package entities;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.*;
 public class Camera {
-    private Vector3f position = new Vector3f(0,0,0);
-    private float pitch;
+    private Vector3f position = new Vector3f(0,10,0);
+    private float pitch = 10;
     private float yaw;
     private float roll;
     public Camera() {
     }
     public void move(){
         if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)){
-            position.y -= 0.02f;
+            position.y -= 0.2f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_UP)){
-            position.y += 0.02f;
+            position.y += 0.2f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_S)){
-            position.z += 0.02f;
+            position.z += 0.2f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-            position.z -= 0.02f;
+            position.z -= 0.2f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-            position.x += 0.02f;
+            position.x += 0.2f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-            position.x -= 0.02f;
+            position.x -= 0.2f;
         }
     }
     public Vector3f getPosition() {
